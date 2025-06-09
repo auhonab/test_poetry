d8520bd61926ecd515f602683c78856e484f0316
Auhona Basu <auhonabasu03@gmail.com>
2025-06-09 09:25:37 -0400

    Update random.py

--- src/test_poetry/random.py
+++ src/test_poetry/random.py
@@ -2,9 +2,5 @@ def add_numbers(a, b):
     return a + b
 
 
-<<<<<<< Updated upstream
-result = add_numbers(2, 3)
-=======
 result = add_numbers(2, 4)
->>>>>>> Stashed changes
 print(f"The result is: {result}")
